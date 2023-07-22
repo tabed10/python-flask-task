f3a319b49b48a4c890af0f39be34630361e49642
Sabbir Hossain Abed <sabbirhossainabed@Sabbirs-MacBook-Pro.local>
2023-07-22 19:19:57 +0600

    add: reading get and post api config

--- db.py
+++ db.py
@@ -1,21 +1,31 @@
-class Reading:
-    # TODO: change this to represent whatever information is needed
-    pass
+from abc import ABC
+from typing import Optional
+from datetime import datetime
+
+
+class Reading(ABC):
+    def __int__(self, time: str, name: str, value: float):
+        self.time = time
+        self.name = name
+        self.value = value
+
+    def to_dict(self):
+        return self.__dict__
 
 
 # This is a fake database which stores data in-memory while the process is running
 # Feel free to change the data structure to anything else you would like
-database: dict[str, Reading] = {}
+database: dict[str, dict] = {}
 
 
 def add_reading(key: str, reading: Reading) -> None:
     """
     Store a reading in the database using the given key
     """
-    database[key] = reading
+    database[key] = reading.to_dict()
 
 
-def get_reading(key: str) -> Reading | None:
+def get_reading(key: str) -> Optional[Reading]:
     """
     Retrieve a reading from the database using the given key
     """
--- main.py
+++ main.py
@@ -1,24 +1,59 @@
-from flask import Flask
-
-from db import get_reading, add_reading
+from flask import Flask, request
+import re
+from db import get_reading, add_reading, Reading, database
+from datetime import datetime
 
 app = Flask(__name__)
 
 
 @app.post("/data")
 def post_data():
-    # TODO: parse incoming data, and save it to the database
-    # data is of the form:
-    #  {timestamp} {name} {value}
+    if request.content_type == 'text/plain':
+        data = request.data.decode('utf-8')
+
+        # validate data
+        lines = data.split('\n')
+        pattern = r'^\d+ (Voltage|Current) \d+(\.\d+)?$'
+        valid_lines = []
+        for line in lines:
+            if line.strip() and re.match(pattern, line):
+                valid_lines.append(line)
+            else:
+                return {"success": False}
+
+        # insert validated data
+        for valid_line in valid_lines:
+            timestamp_str, name, value_str = valid_line.split()
+            timestamp = int(timestamp_str)
+            dt_object = datetime.utcfromtimestamp(timestamp)
+            formatted_date = dt_object.strftime("%Y-%m-%dT%H:%M:%S.%fZ")
+
+            reading_obj = Reading(time=formatted_date, name=name, value=float(value_str))
+            add_reading(formatted_date, reading_obj)
+    else:
+        return {"success": False}
 
     return {"success": False}
 
 
 @app.get("/data")
 def get_data():
-    # TODO: check what dates have been requested, and retrieve all data within the given range
+    from_date_str = request.args.get('from')
+    to_date_str = request.args.get('to')
 
-    return {"success": False}
+    if not from_date_str or not to_date_str:
+        return "Invalid request"
+
+    from_date = datetime.strptime(from_date_str, "%Y-%m-%d")
+    to_date = datetime.strptime(to_date_str, "%Y-%m-%d")
+
+    results = []
+    for key, data in database.items():
+        timestamp_date = data['timestamp']
+        if from_date <= timestamp_date <= to_date:
+            results.append(data)
+
+    return results
 
 
 if __name__ == "__main__":
